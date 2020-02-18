acea116bffe152bb2d6aeceac2a1564e783d06d9
admin <cuong251095@gmail.com>
2020-02-18 12:46:49 +0700

    init project

---
+++ Learning_Git/Learning_Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learning_Git
+//
+//  Created by admin on 2/18/20.
+//  Copyright © 2020 dev.pro.cuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 * this is developer 1.
+* this is developer 2.
 * this is developer 3.
